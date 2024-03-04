de9ec552d714d14b7069d57c626704d140b55a3f
SassId <redrednanaki@gmail.com>
2024-03-04 16:58:49 +0100

    added exercices

--- javascript/assets/js/07-loops.js
+++ javascript/assets/js/07-loops.js
@@ -2,15 +2,21 @@
 
 //* Loops are code structures allowing us to execute instructions multiples times
 
-const wholeNumber = prompt("pick a number between 1 and 10")
-let star = '*'
-if (wholeNumber > 0 && wholeNumber <= 10) {
-for (let i = 0; i <wholeNumber; i++ ) {
-        console.log(star)
-        star = star + '*'
-    }
-    
-} else {
-    console.log("i said between 1 and 10")
-    wholeNumber = prompt("pick a number between 1 and 10")
+const wholeNumber = prompt("pick a number between 1 and 10");
+let star = "*";
+// if (wholeNumber > 0 && wholeNumber <= 10) {
+//   for (let i = 0; i < wholeNumber; i++) {
+//     console.log(star);
+//     star = star + "*";
+//   }
+// } else {
+//   console.log("i said between 1 and 10");
+//   wholeNumber = prompt("pick a number between 1 and 10");
+// }
+
+while (wholeNumber > 0 && wholeNumber <= 10 ) {
+    let i = 0; 
+    console.log(star)
+    i++;
+    star = star + "*"
 }
