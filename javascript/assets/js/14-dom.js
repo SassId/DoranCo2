50cbe1e8f2d2fc98e7d99ec3de96401fd7861723
SassId <redrednanaki@gmail.com>
2024-03-06 15:16:11 +0100

    added DOM definitions

--- javascript/assets/js/14-dom.js
+++ javascript/assets/js/14-dom.js
@@ -1,1 +1,21 @@
-console.log(5454);
+/* ----------------------- WINDOW -----------------------*/
+
+//* The DOM (Document Objet Model) in JS is a memory (ram) based represention of a HTML document loaded by the browser.
+//* It represents each object of the document as a modifiable JS object that can be manipulated and modified trough the JS code.
+
+// -----------------------------------------------------------
+//                    JS SELECTORS METHODS
+// -----------------------------------------------------------
+
+
+const mainTitle=  document.getElementById('main-title')
+mainTitle.style.color = "red"
+mainTitle.style.fontFamily = "Arial"
+console.log(mainTitle);
+
+const paragraphs = document.getElementsByTagName('p')
+for (let i = 0; i < paragraphs.length; i++) {
+    paragraphs[i].style.color = "blue"
+}
+
+console.log(paragraphs);
