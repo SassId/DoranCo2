75f83c86f527b232a4adc258e0fc398edb3f0b65
SassId <redrednanaki@gmail.com>
2024-03-08 16:48:32 +0100

    form practice sequel

--- javascript/practice/form/js/index.js
+++ javascript/practice/form/js/index.js
@@ -6,12 +6,15 @@ submitButton.addEventListener('click', (event) => {
     event.preventDefault()
 
     const nameInput = document.querySelector('#name');
-    console.log(nameInput.parentElement);
+    // console.log(nameInput.parentElement);
     const emailInput = document.querySelector('#email');
     const passwordInput = document.querySelector('#password');
     const confirmPassword = document.querySelector('#confirm-password')
 
     if (nameInput.value == '') {
+        const errorMessage1 = document.createElement('p');
+        errorMessage1.textContent = "Please enter your name";
+        errorMessage1.classList.add('error-message');
         
 
     }
