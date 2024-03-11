f59bb6c6f93d617c00ce02a3071fdb192eab34bf
SassId <redrednanaki@gmail.com>
2024-03-11 21:22:20 +0100

    added form exercice

--- javascript/practice/form/js/index.js
+++ javascript/practice/form/js/index.js
@@ -1,21 +1,24 @@
-console.log(654654);
-const submitButton = document.querySelector('#submit-button')
+const submitButton = document.querySelector("#submit-button");
 console.log(submitButton);
 
-submitButton.addEventListener('click', (event) => {
-    event.preventDefault()
+submitButton.addEventListener("click", (event) => {
+  event.preventDefault();
 
-    const nameInput = document.querySelector('#name');
-    // console.log(nameInput.parentElement);
-    const emailInput = document.querySelector('#email');
-    const passwordInput = document.querySelector('#password');
-    const confirmPassword = document.querySelector('#confirm-password')
+  const nameInput = document.querySelector("#name");
+  console.log(nameInput.parentElement);
+  const emailInput = document.querySelector("#email");
+  const passwordInput = document.querySelector("#password");
+  const confirmPassword = document.querySelector("#confirm-password");
 
-    if (nameInput.value == '') {
-        const errorMessage1 = document.createElement('p');
-        errorMessage1.textContent = "Please enter your name";
-        errorMessage1.classList.add('error-message');
-        
-
+  if (nameInput.value == "") {
+    if (errorMessage1) {
+      errorMessage1.remove;
     }
-})
+    const errorMessage1 = document.createElement("p");
+    errorMessage1.textContent = "Please enter your name";
+    errorMessage1.classList.add("error-message");
+    errorMessage1.setAttribute("id", "PlsEnterName");
+
+    nameInput.parentElement.append(errorMessage1);
+  }
+});
