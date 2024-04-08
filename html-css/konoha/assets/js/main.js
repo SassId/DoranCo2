5c4b5a8a58002f493593be31bb278473725462d4
sassid <redrednanaki@gmail.com>
2024-04-08 11:04:03 +0200

    added new functionalies in konoha

--- html-css/konoha/assets/js/main.js
+++ html-css/konoha/assets/js/main.js
@@ -1,9 +1,23 @@
-//todo 
+//todo
 //todo when i click on the konoha logo
 
-const logoLink = document.getElementById('logo-link');
+// * First Method: if a condition
+// const burgerIcon = document.getElementById("burger-icon");
 
-logoLink.addEventListener('click', () => {
+// burgerIcon.addEventListener("click", () => {
+//     const navigation = document.getElementById("navigation");
+    
+//     if (navigation.classList.contains("active")) {
+//         navigation.classList.remove("active");
+//     } else {
+//         navigation.classList.add("active");
+//     }
+// });
+
+// * Second Method: with the toggle method:
+
+const burgerIcon = document.getElementById('burger-icon');
+burgerIcon.addEventListener('click', () => {
     const navigation = document.getElementById('navigation');
     navigation.classList.toggle('active')
 })
