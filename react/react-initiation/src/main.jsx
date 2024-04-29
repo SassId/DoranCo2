3d655388ceaaca94db866bb716d1194cb1ca2139
sassid <redrednanaki@gmail.com>
2024-04-29 17:03:39 +0200

    added comments

---
+++ react/react-initiation/src/main.jsx
@@ -0,0 +1,12 @@
+// * Library to manage the states 
+import React from 'react'
+// *Library to render the application in the html et to updtate elements
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
