beaa6c830da8fb86f1c7dc936e0c8ab0e5afca3d
sassid <redrednanaki@gmail.com>
2024-04-30 16:57:38 +0200

    done for today

--- react/react-initiation/src/components/ui-atoms/titre/titre.jsx
+++ react/react-initiation/src/components/ui-atoms/titre/titre.jsx
@@ -1,5 +1,5 @@
-import myClass from "./titre.module.css"
+import myClass from "./titre.module.css";
 
-export default function Titre({children}){
-return <h1>{children}</h1>
+export default function Titre({ children }) {
+  return <h1>{children}</h1>;
 }
