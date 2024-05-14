36021e5ab8443883d77d83b080eea0c386cb2d40
sassid <redrednanaki@gmail.com>
2024-05-14 10:50:45 +0200

    added new express exercice

--- node_js/express_js/src/index.js
+++ node_js/express_js/src/index.js
@@ -8,10 +8,6 @@ server.get("/api/posts", (req, res) => {
   res.end("Hello");
 });
 
-server.listen(3005, function () {
-  console.log("Server on port 3005");
-});
-
 // TODO:
 // Exercice:
 // Ajouter un handler pour l'url "/api/users" avec la methode POST
@@ -48,4 +44,66 @@ server.get("/api/todos", (req, res) => {
     // equals to
     res.json(JSON.parse(data.toString()));
   });
+});
+
+// * To collect info from the url:
+// * ex: /api/user?firstname=Doe&lastname=John
+server.get("/api/user", (req, res) => {
+  const data = req.query;
+  console.log(data); // data is then an object containing firstname and lastname as keys and value
+
+  if (!data.firstname || !data.lastname) {
+    return res.status(400).json({ error: "mandatory information" });
+  }
+
+  res.json(data);
+});
+
+// TODO:
+// Exercice:
+// Ajouter un handler pour l'url "/api/todo":
+// /api/todo?id=2
+// /api/todo?id=4
+// Récuperer la liste des todos
+// Si la todo n'existe pas , retourner un 404
+// Sinon Retourner en JSON la todo avec l'id fournit
+
+server.get("/api/todo", (req, res) => {
+  const todoData = req.query;
+  console.log(todoData);
+
+  // check if the id in the url doesn't exist
+  if (todoData.id === undefined) {
+    return res.status(404).json({ error: "page not found, sorry!" });
+  }
+  // read the json file used as database
+  fs.readFile("./src/data/todos.json", (err, data) => {
+    if (err) {
+      res.json({ error: "something wrong has occured" });
+      res.end("cannot read file");
+      return;
+    }
+    // Changes the buffer obtained after reading the file into a string and then a js object
+    const todoItem = JSON.parse(data.toString());
+    console.log(todoItem);
+
+    // we now have an object (todoItem) containing a key (todos) that is an array containing an object
+    // Filter the array to match the condition
+
+    // here, item is an object
+    const filterData = todoItem.todos.filter((item) => item.id == todoData.id);
+    console.log(filterData);
+    // Check if the array is empty
+    if (filterData.length === 0) {
+      res.status(404).json({ error: "page not found" });
+      return;
+    }
+    console.log(filterData[0]);
+    // after the filter, we get a single value that is still an array
+    return res.json(filterData[0]);
+  });
+});
+
+server.listen(3005, function () {
+  console.log("Server on port 3005");
 });
