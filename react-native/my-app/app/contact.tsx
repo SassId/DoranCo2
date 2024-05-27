b3a64f4731666d39164a9cb6bd53cc8ea3f682ac
sassid <redrednanaki@gmail.com>
2024-05-27 16:26:55 +0200

    added components and their props

--- react-native/my-app/app/contact.tsx
+++ react-native/my-app/app/contact.tsx
@@ -1,10 +1,92 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import { useState } from "react";
+import React from "react";
+import { StyleSheet, Text, View, TextInput, Alert } from "react-native";
+import { contactStyle } from "../styles/contact";
+import Button from "@/components/Button/Button";
+import InputWithError from "@/components/InputWithError/InputWithErro";
 
 export default function contact() {
+  const [emailInput, setEmailInput] = useState("");
+  const [emailError, setEmailError] = useState("");
+
+  // ! We need to type the props
+  function handleEmail(text: string) {
+    setEmailError("");
+    setEmailInput(text);
+    if (text == "") {
+      return setEmailError("please enter your email");
+    }
+    if (!text.includes("@")) {
+      setEmailError("please enter a valid email");
+    }
+  }
+
+  const [messageInput, setMessageInput] = useState("");
+  const [messageError, setMessageError] = useState("");
+
+  function handleMessage(text: string) {
+    setMessageError("");
+    setMessageInput(text);
+    if (text == "") {
+      setMessageError("please enter your message");
+    }
+  }
+
+  function submit() {
+    alert(messageInput);
+  }
+
   return (
     <View>
-      <Text>Hello</Text>
+      <Text style={contactStyle.title1}>Join Us Today !</Text>
+      <View style={contactStyle.formWrapper}>
+        <InputWithError
+          placeholder="enter your email"
+          error={emailError}
+          action={(text) => handleEmail(text)}
+          type="default"
+        ></InputWithError>
+        <InputWithError
+          placeholder="enter your message"
+          error={messageError}
+          action={(text) => handleMessage(text)}
+          type="default"
+        ></InputWithError>
+
+        {/* <TextInput
+          style={contactStyle.inputs}
+          placeholder="enter your email"
+          keyboardType="email-address"
+          onChangeText={handleEmail}
+        ></TextInput>
+        <Text>{emailError}</Text>
+        <TextInput
+          style={contactStyle.inputs}
+          placeholder="enter your message"
+          multiline={true}
+          numberOfLines={5}
+          onChangeText={handleMessage}
+        ></TextInput> */}
+        <Text>{messageError}</Text>
+        <Button style={contactStyle} label="Submit" action={submit}></Button>
+      </View>
     </View>
-  )
+  );
 }
+
+// TODO:
+// Exercice:
+// Créer un formulaire de contact avec:
+// Email: obligatoire sinon message d'erreur
+// Message: obligatoire sinon message d'erreurs
+// Un bouton pour valider le formulaire
+// Quand on clique sur le bouton, afficher une alerte avec le message
+// Styliser le formulaire
+
+// TODO:
+// Exercice:
+// Créer le composant InputAvecError (/components/InputAvecError/InputAvecError.jsx):
+// Le composant reçoit 4 props:
+// Le placeholder, action(fonction a lier avec le onChangeText), type (Le type de clavie), error (Le message d'erreure a afficher sous l'input)
+
+// Remplacer le composant input et son erreur dans contact.jsx
