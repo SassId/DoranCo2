64ea0cc52884c423e5c47ad30286c5a46537277c
sassid <redrednanaki@gmail.com>
2024-05-31 17:00:40 +0200

    added new app in react

--- react/@latest/src/App.jsx
+++ react/@latest/src/App.jsx
@@ -1,12 +1,25 @@
 import "./globals.css";
+import { changeTheme } from "./lib/theme";
+import { Button } from "./components/ui/button";
+import { CarouselSize } from "./components/mine/carousel-main";
 
 function App() {
   return (
     <>
-      <main className="h-screen w-screen bg-neutral-100 md:bg-blue-500">
-        <h1 className="text-4xl text-red-500 md:text-white hover:text-blue-500">
-          Hello, World !
-        </h1>
+      <main className="bg-background">
+        <h1 className="text-foreground text-center">Hello, World !</h1>
+        <Button onClick={changeTheme} variant="destructive">
+          Change Theme
+        </Button>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas
+          ipsa saepe pariatur delectus modi assumenda blanditiis esse
+          repudiandae ipsum in harum fugiat quod, inventore sapiente, tempore
+          expedita animi porro veritatis provident! Harum dolorum labore
+          explicabo repellat architecto dolores dolore natus, dolorem qui
+          numquam praesentium delectus ratione. Eos dolorem earum quos?
+        </p>
+        <CarouselSize />
       </main>
     </>
   );
