92e119beed19baf2459ec63baf95f1765432dde9
sassid <redrednanaki@gmail.com>
2024-05-31 11:23:17 +0200

    config tailwind and added globals.css

---
+++ react/@latest/src/App.jsx
@@ -0,0 +1,15 @@
+import "./globals.css";
+
+function App() {
+  return (
+    <>
+      <main className="h-screen w-screen bg-neutral-100 md:bg-blue-500">
+        <h1 className="text-4xl text-red-500 md:text-white hover:text-blue-500">
+          Hello, World !
+        </h1>
+      </main>
+    </>
+  );
+}
+
+export default App;
