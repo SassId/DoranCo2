ba007fa3201e730288351b1623eedc5eda8228c4
assadcr <assadoucs75020@gmail.com>
2024-07-15 17:01:12 +0200

    added new dir

--- symfony_learning/src/Controller/HomeController.php
+++ symfony_learning/src/Controller/HomeController.php
@@ -2,12 +2,13 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Routing\Requirement\Requirement;
 
-class HomeController
+class HomeController extends AbstractController
 {
     #[Route('/', name: 'home_index')]
     public function index(): Response
@@ -31,5 +32,20 @@ class HomeController
     public function greetings(int $age): Response
     {
         return new Response('Hi, I\'m ' . $age . ' years old.');
+    }
+
+    // relates to the other way to create a route (routes.yaml)
+    public function list(): Response
+    {
+        return new Response('Hi, I\'m a route created in yaml.');
+    }
+
+    #[Route('/template/{test}', name: 'home_templates')]
+    public function template(string $test): Response
+    {
+
+        $animals = ['lions', 'tigers', 'wolves', 'eagles', 'goats'];
+
+        return $this->render('home/index.html.twig', ['name' => 'Roo', 'key' => $test, 'animals' => $animals]);
     }
 }
