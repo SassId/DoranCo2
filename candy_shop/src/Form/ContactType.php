8e0c840f4cb4880ac3faca59e4c6802accce5241
SassId <redrednanaki@gmail.com>
2024-07-21 16:55:58 +0200

    added js

--- candy_shop/src/Form/ContactType.php
+++ candy_shop/src/Form/ContactType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\DTO\ContactDTO;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,7 +16,7 @@ class ContactType extends AbstractType
         $builder
             ->add('name')
             ->add('email')
-            ->add('message')
+            ->add('message', TextareaType::class)
         ;
     }
 
